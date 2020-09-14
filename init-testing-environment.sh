1132ef89a4b857deed32bab596e7b3e3d9bdafe1
Sam Webster <sam@secondhandlegends.com>
2020-09-13 23:10:33 -0400

    Updated docker compose with env variables

---
+++ init-testing-environment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /var/www/html/
+
+#1. check if wordpress is already installed/configured
+if (sudo -u www-data -- wp core is-installed)
+then
+
+	#2. check if the database is ready
+	if ! (sudo -u www-data -- wp db check)
+	then
+		# wait a moment for the database container
+		sleep 1
+		exit 1;
+	fi
+
+	#3. init the testing instance
+	sudo -u www-data -- wp scaffold plugin-tests $WP_PLUGIN_FOLDER --force
+	cd /usr/src/wordpress/wp-content/plugins/gravityforms && bash tests/bin/install.sh wordpress_tests wordpress wordpress db
+fi
+
+#4. back to the root WP folder
+cd /var/www/html/
+
+#5. execute the entrypoint of the parent image
+bash docker-entrypoint.sh "$@"
